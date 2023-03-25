80566844c062196257827137478fd5eed4a79e00
Armaan Gupta <armaan@guppy.im>
2023-03-25 18:10:48 -0500

    Initial Commit

---
+++ GitBar/GitBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBarApp.swift
+//  GitBar
+//
+//  Created by Armaan Gupta on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct GitBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
