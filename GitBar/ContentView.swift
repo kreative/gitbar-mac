a839d179c5015fdd6268a2de7c88c4eb5aa156de
Armaan Gupta <armaan@guppy.im>
2023-03-25 20:39:08 -0500

    ref: made a basic view

--- GitBar/ContentView.swift
+++ GitBar/ContentView.swift
@@ -5,4 +5,28 @@
 //  Created by Armaan Gupta on 3/25/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Your Stats")
+                .font(.headline)
+            Text("21 Today")
+            Divider()
+            Button(action: {
+                NSApplication.shared.terminate(self)
+            }) {
+                Text("Quit")
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
